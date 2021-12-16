5d25ada7e9bf58b7185a131bd95d159d541eaa09
Eduardo Cadavid <87548701+ECG301997@users.noreply.github.com>
2021-12-15 23:07:00 -0500

    Delete app directory

--- app/src/main/java/com/volcanapp/volcanapp/modelos/FirebaseReference.java
+++
@@ -1,7 +0,0 @@
-package com.volcanapp.volcanapp.modelos;
-
-public class FirebaseReference {
-    final public static String DB_REFERENCE_LUGARES_MONITOREADOS = "lugares_monitoreados";
-    final public static String DB_REFERENCE_USERS = "usuarios";
-    final public static String DB_REFERENCE_ALERT = "alertas";
-}
